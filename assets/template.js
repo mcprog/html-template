e805e6e593e8bcdd63ad92cb025d85e1163a20a0
DD-EDH-N6 <michaelcurtis96@gmail.com>
2017-08-04 15:53:34 -0700

    chips close. used jq on instead of click (looks at uncreate elements too)

--- assets/template.js
+++ assets/template.js
@@ -5,4 +5,22 @@ $(document).ready(function() {
   var query = url.substring(url.indexOf('?') + 1);
   console.log(query);
   $('.nav-link[data-id="' + query + '"]').addClass("active");
+
+  $('#addPatternButton').click(function() {
+    var searchFor = $('#searchFor').val();
+    if (!searchFor) {
+      $('#patternModal').modal('show');
+      return;
+    }
+    addPatten(searchFor, $('#replaceWith').val());
+  });
+
+  $(document).on('click', '.close-chip', function() {
+    $(this).closest('.chip').remove();
+  });
 });
+
+
+function addPatten(search, replace) {
+  $('#patternList').append('<div class="chip bg-info text-white">[' + search + '], [' + replace + ']&nbsp;&nbsp;<span class="close-chip text-white" aria-hidden="true">&times;</span></div>');
+}
